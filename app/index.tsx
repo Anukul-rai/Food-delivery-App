ac08e324c03e27dba72fb5d734964c68c385bc6e
anukul rai <anukul.rau@gmail.com>
2025-07-30 23:53:52 +0545

    start

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>My new app for food delivery</Text>
     </View>
   );
 }
