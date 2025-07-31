0d30395f5aae71c22222c80c5483df50d45a1df1
anukul rai <anukul.rau@gmail.com>
2025-07-31 23:44:30 +0545

    update the components

--- app/homescreen.tsx
+++ app/homescreen.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function homescreen() {
+export default function Homescreen() {
   return (
     <View>
-      <Text>homescreen</Text>
+      <Text>H</Text>
     </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import Homescreen from "./homescreen";
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>My new app for Food delivery</Text>
+      <Homescreen/>
     </View>
   );
 }
