06b09d4affc685084e9da30f180a7d1ad522c725
anukul rai <anukul.rau@gmail.com>
2025-08-02 22:26:45 +0545

    add some cahnges

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,12 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { categories } from '@/constants'
+
 
 export default function Categories() {
-  return (
-    <View>
-      <Text>Categories</Text>
-    </View>
-  )
+    return (
+        <View style={{marginTop:4}}>
+        <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+            paddingHorizontal:15
+        }}
+        style={{overflow:'visible'}}
+        >
+            {
+                categories.map((category,index)=>(
+                    <View key={index} style={styles.categoryCont}>
+                        <TouchableOpacity 
+                        style={styles.catergories}>
+                            <Text>{category.name}</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))
+            }
+
+        </ScrollView>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    categoryCont:{
+        justifyContent:'center',
+        alignItems:'center',
+        marginRight:6
+    },
+    catergories:{
+        padding:1,
+        borderRadius:20,
+        backgroundColor:'#ef23'
+    }
+})
