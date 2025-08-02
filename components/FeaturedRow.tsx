4ef6dc5637aa7739e9704d396829f41e130996bf
anukul rai <anukul.rau@gmail.com>
2025-08-03 00:26:18 +0545

    changes

--- components/FeaturedRow.tsx
+++ components/FeaturedRow.tsx
@@ -1,18 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 interface Feature{
   title:number,
   restaurants:string,
-  description:string,
+  description:any,
 }
 
 export default function FeaturedRow({title,restaurants,description}:Feature) {
   return (
     <View>
-      <View>
-        <Text>{title}</Text>
-        <Text>{description}</Text>
+      <View style={{flexDirection:'row', justifyContent:'space-between',alignItems:'center', paddingHorizontal:7}}>
+        <View>
+          <Text style={{fontSize:15,fontWeight:'bold'}}>{title}</Text>
+          <Text style={{fontSize:10}}>{description}</Text>
+        </View>
+      <TouchableOpacity>
+        <Text style={{fontSize:14,color:'green',fontWeight:500}}>See All</Text>
+      </TouchableOpacity>
       </View>
     </View>
   )
